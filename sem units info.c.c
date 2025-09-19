b719dc8415eaf898dd3a8aabb90926896b1e518e
Daniel Nyakeramba <danielonsocho@gmail.com>
2025-09-20 02:41:21 +0300

    Add files via upload

---
+++ sem units info.c.c
@@ -0,0 +1,12 @@
+#include<stdio.h> 
+
+int main(){
+	printf("discrete mathematics\n");
+	printf("mathematics for science\n");
+	printf("fundamentals of ICT\n");
+	printf("computer system and organization\n");
+	printf("communication skills\n");
+	printf("introduction to programming and problem solving\n");
+	printf("fundamentals of system analysis and design\n");
+	   return 0;
+}	   
